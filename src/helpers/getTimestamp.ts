971e58411a8b4c7ce67725a177287ebdb03db27c
JadeBetty <jadebetty.7261@gmail.com>
2026-04-05 20:51:33 +0700

    fix: force12h timestamp for conversion input (#16)
    The raw timestamp fed into converTime could be of a different format than supported on specific machines; now fixed.

--- src/helpers/getTimestamp.ts
+++ src/helpers/getTimestamp.ts
@@ -1,7 +1,7 @@
 import { convertTime } from '.';
 
 export const getTimestamp = (): string => {
-  const time = convertTime(new Date().toLocaleTimeString());
+  const time = convertTime(new Date().toLocaleTimeString('en-US', { hour12: true })) || '00:00:00';
   // const date = new Date().toLocaleDateString()
   const date = new Date().toLocaleDateString('default', { month: 'numeric', day: 'numeric' });
 
